7b5908b48b399a71cf900e30a940672e87876028
KokaKiwi <admin@kokabsolu.com>
2011-11-20 10:55:53 +0100

    Now compile with JavaSE1.5

--- src/main/java/com/kowymaker/client/KowyMakerClient.java
+++ src/main/java/com/kowymaker/client/KowyMakerClient.java
@@ -1,89 +1,65 @@
-/**
- *  This file is part of Kowy Maker - Client.
- *
- *  Kowy Maker - Client is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Kowy Maker - Client is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Kowy Maker - Client.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.kowymaker.client;
 
-import com.kowymaker.client.graphics.GameWindow;
-import com.kowymaker.spec.console.ConsoleOutputManager;
+import java.io.File;
+
+import com.kowymaker.client.graphics.ClientWindow;
 import com.kowymaker.spec.utils.Configuration;
+import com.kowymaker.spec.utils.SystemUtils;
 
 public class KowyMakerClient
 {
     private final Configuration configuration = new Configuration();
-    private final GameWindow    window;
     
-    /**
-     * Initialize variables and resources.
-     */
+    private final ClientWindow  window;
+    
     public KowyMakerClient()
     {
-        ConsoleOutputManager.register("client");
-        
-        // Initialize configuration
         try
         {
             configuration.load(KowyMakerClient.class
                     .getResourceAsStream("/config/config.yml"), "yaml");
         }
-        catch (final Exception e)
+        catch (Exception e)
         {
             e.printStackTrace();
         }
         
-        // Load resources
+        File nativesDir = new File("natives" + File.separator
+                + SystemUtils.getSystemOS().name());
+        System.setProperty("org.lwjgl.librarypath",
+                nativesDir.getAbsolutePath());
+        System.setProperty("net.java.games.input.librarypath",
+                nativesDir.getAbsolutePath());
         
-        // Initialize window
-        window = new GameWindow(this);
-        window.init();
+        window = new ClientWindow(this);
+        window.getEngine().getConfiguration().setCenterOrigin(true);
         
-        // Initialize interface
-        
+        Test test = new Test();
+        window.getEngine().addChild(test);
     }
     
-    /**
-     * Start Kowy Maker.
-     */
-    public void start()
-    {
-        //Show window
-        window.open();
-    }
-    
-    /**
-     * Stop Kowy Maker.
-     */
-    public void stop()
-    {
-        window.close();
-        System.exit(0);
-    }
-    
-    /**
-     * Get Client configuration.
-     * 
-     * @return Client configuration
-     */
     public Configuration getConfiguration()
     {
         return configuration;
     }
     
+    public void start()
+    {
+        window.setVisible(true);
+    }
+    
+    public void stop()
+    {
+        window.setVisible(false);
+        System.exit(0);
+    }
+    
+    /**
+     * @param args
+     */
     public static void main(String[] args)
     {
-        final KowyMakerClient client = new KowyMakerClient();
+        KowyMakerClient client = new KowyMakerClient();
         client.start();
     }
     
